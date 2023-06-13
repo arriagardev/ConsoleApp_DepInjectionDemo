79e0c6bbb754000898aa20c67b7eae33139a24e7
Humberto Arriaga <harriaga@rehrig.com>
2023-06-13 01:00:00 -0600

    test log

--- ConsoleApp_DepInjectionDemo/NumberWorker.cs
+++ ConsoleApp_DepInjectionDemo/NumberWorker.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,12 +10,20 @@ namespace ConsoleApp_DepInjectionDemo
     public class NumberWorker
     {
         private readonly INumberService _service;
+        private readonly ILogger _logger;
 
-        public NumberWorker(INumberService service) => _service = service;
+        public NumberWorker(
+            INumberService service,
+            ILogger<NumberWorker> logger)
+        {
+            _service = service;
+            _logger = logger;
+        }
 
         public void PrintNumber()
         {
             var number = _service.GetPositiveNumber();
+            _logger.LogInformation($"The number I found is {number}");
             Console.WriteLine($"My wonderful number is {number}");
         }
 
